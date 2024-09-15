23d90a9e6265be5a32f08afc35bf0aaa1758abcd
reichikku <bladeryz@gmail.com>
2024-09-15 21:36:19 +0300

    Fixes

--- cinema/views.py
+++ cinema/views.py
@@ -1,6 +1,11 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import status, generics, mixins, viewsets
+from rest_framework import (
+    status,
+    generics,
+    mixins,
+    viewsets
+)
 
 from django.shortcuts import get_object_or_404
 
